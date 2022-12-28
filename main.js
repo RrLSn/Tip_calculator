63d83f08bf48938225b168f82b45d9b278a87888
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-28 17:27:42 +0100

    7

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 // Selectors
 const warningEl = document.querySelector('.warn')
+
 const billEl = document.querySelector('#bill')
 const sharingBill = document.querySelector('#bill-sharing')
+const selectEl = document.querySelector('#select')
+const calEl = document.querySelector('#cal')
+
 const billWarn = document.querySelector('.bill-warn')
 const sharingWarn = document.querySelector('.sharing-warn')
 const serviceWarn = document.querySelector('.service-warn')
+
+// const option = document.querySelectorAll('option')
+
+const answer = document.querySelector('.answer')
 const tipAmnt = document.querySelector('.tip-Amnt')
 const billAmnt = document.querySelector('.bill-Amnt')
 const owesEl = document.querySelector('.owes')
-const calEl = document.querySelector('#cal')
-const selectEl = document.querySelector('#select')
-const option = document.querySelectorAll('option')
+
+warningEl.style.lineHeight = '1.7rem'
+
 
 // Event Listener
 calEl.addEventListener(
@@ -19,7 +27,7 @@ calEl.addEventListener(
         if(billEl.value === ''){
             warningEl.style.display = 'block'
             billWarn.textContent = 'Bill Amount Cannot Be Blank'
-        }else{
+        }else {
             warningEl.style.display = 'none'
             billWarn.textContent = ''
         }
@@ -30,11 +38,39 @@ calEl.addEventListener(
             warningEl.style.display = 'none'
             sharingWarn.textContent = ''
         }
-
-        if(selectEl.value === option[0]){
-            console.log(option[0])
-            // warningEl.style.display = 'block'
-            // serviceWarn.textContent = 'grow'
+        if (selectEl.value === "Choose"){
+            warningEl.style.display = "block";
+            serviceWarn.textContent = "You Must Select A Service";
+        }else{
+            warningEl.style.display = 'none'
+            serviceWarn.textContent = ''
+        }
+        if(selectEl.value === "Great"){
+            answer.style.display = 'block'
+            let grBill = 0.2 * billEl.value
+            let total = grBill + parseInt(billEl.value)
+            let owes = total / parseInt(sharingBill.value)
+            tipAmnt.textContent = `Tip Amount $${grBill}.00`
+            billAmnt.textContent = `Total Amount $${total}.00`
+            owesEl.textContent = `Each Person Owes $${owes}.00`
+        }
+        else if(selectEl.value === "Good"){
+            answer.style.display = 'block'
+            let gBill = 0.1 * billEl.value
+            let total = gBill + parseInt(billEl.value)
+            let owes2 = total / parseInt(sharingBill.value)
+            tipAmnt.textContent = `Tip Amount $${gBill}.00`
+            billAmnt.textContent = `Total Amount $${total}.00`
+            owesEl.textContent = `Each Person Owes $${owes2}.00`
+        }
+        else if(selectEl.value === 'Bad'){
+            answer.style.display = 'block'
+            let bBill = 0.02 * billEl.value
+            let total = bBill + parseInt(billEl.value)
+            let owes3 = total / parseInt(sharingBill.value)
+            tipAmnt.textContent = `Tip Amount $${bBill}.00`
+            billAmnt.textContent = `Total Amount $${total}.00`
+            owesEl.textContent = `Each Person Owes $${owes3}.00`
         }
     }
 )
